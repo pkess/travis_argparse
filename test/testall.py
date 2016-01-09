5580025ffa4cf3a57e7f6cb2d091a8c94c729cf5
Peter Kessen <p.kessen@kessen-peter.de>
2016-01-09 12:28:49 +0100

    init

---
+++ test/testall.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of easydms.
+# Copyright (c) 2015 Peter Kessen
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject
+# to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+import sys
+import os
+import re
+
+from _common import unittest
+
+pkgpath = os.path.dirname(__file__) or '.'
+sys.path.append(pkgpath)
+os.chdir(pkgpath)
+
+
+def suite():
+    s = unittest.TestSuite()
+    # Get the suite() of every module in this directory beginning with
+    # "test_".
+    for fname in os.listdir(pkgpath):
+        match = re.match(r'(test_\S+)\.py$', fname)
+        if match:
+            modname = match.group(1)
+            s.addTest(__import__(modname).suite())
+    return s
+
+if __name__ == b'__main__':
+    unittest.main(defaultTest='suite')
